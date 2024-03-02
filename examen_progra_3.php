c756c5060da2473a98c8a010942ebd0ef7bd4775
practus11 <enamoradojr11@gmail.com>
2024-03-02 12:30:14 -0600

    numero de 3 en 3 hasta llegar a 30

--- examen_progra_3.php
+++ examen_progra_3.php
@@ -12,5 +12,11 @@ for ($i = 0; $i <= 50; $i++) {
 
 echo "La suma de los números pares en el rango de 0 a 50 es: " . $b . "<br>";
 echo "La suma de los números impares en el rango de 0 a 50 es: " . $a . "<br><br>";
+$num = 0;
 
+while ($num <= 30 || $num % 2 == 0) {
+    $num += 3;
+}
+
+echo "El primer número impar después de 30 es: " . $num;
 ?>
